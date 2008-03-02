047a4fc65783449035716cde6eadfbce073b06f7
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2008-03-02 01:49:25 +0000

    update test to actually test results
    git-svn-id: https://code.astraw.com/motmot/trunk/realtime_image_analysis@508 9608957c-362e-0410-90bb-ddb82fccb57f

--- motmot/realtime_image_analysis/tests.py
+++ motmot/realtime_image_analysis/tests.py
@@ -3,6 +3,27 @@ import motmot.realtime_image_analysis.realtime_image_analysis as ria
 import motmot.FastImage.FastImage as FastImage
 import numpy
 
+XT = 67
+YT = 31
+
+def result_validator_func(points):
+    assert len(points)==1
+    for pt in points:
+        (x, y, area, slope, eccentricity) = pt
+        if abs(x-XT) > 0.01:
+            raise ValueError('x not close to %d'%XT)
+        if abs(y-YT) > 0.01:
+            raise ValueError('y not close to %d'%YT)
+    return
+
+def get_roi_im(w,h):
+    sz = FastImage.Size(w,h)
+    roi_im=FastImage.FastImage8u(sz)
+    arr = numpy.asarray(roi_im) # numpy view
+    arr[:,:] = 0
+    arr[YT,XT] = 50
+    return roi_im, result_validator_func
+
 class TestRealtimeImageAnalysis(unittest.TestCase):
     def setUp(self):
         self.w = 640
@@ -17,36 +38,36 @@ class TestRealtimeImageAnalysis(unittest.TestCase):
         framenumber = 0
         use_roi2 = False
         use_cmp = False
-        sz = FastImage.Size(self.w,self.h)
-        roi_im=FastImage.FastImage8u(sz)
-        self.ra.do_work(roi_im,timestamp,framenumber,use_roi2,use_cmp)
+        roi_im,result_validator_func=get_roi_im(self.w,self.h)
+        points = self.ra.do_work(roi_im,timestamp,framenumber,use_roi2,use_cmp)
+        result_validator_func( points )
 
     def test_basic_roi2(self):
         timestamp = 0.0
         framenumber = 0
         use_roi2 = True
         use_cmp = False
-        sz = FastImage.Size(self.w,self.h)
-        roi_im=FastImage.FastImage8u(sz)
-        self.ra.do_work(roi_im,timestamp,framenumber,use_roi2,use_cmp)
+        roi_im,result_validator_func=get_roi_im(self.w,self.h)
+        points = self.ra.do_work(roi_im,timestamp,framenumber,use_roi2,use_cmp)
+        result_validator_func( points )
 
     def test_variance(self):
         timestamp = 0.0
         framenumber = 0
         use_roi2 = False
         use_cmp = True
-        sz = FastImage.Size(self.w,self.h)
-        roi_im=FastImage.FastImage8u(sz)
-        self.ra.do_work(roi_im,timestamp,framenumber,use_roi2,use_cmp)
+        roi_im,result_validator_func=get_roi_im(self.w,self.h)
+        points = self.ra.do_work(roi_im,timestamp,framenumber,use_roi2,use_cmp)
+        result_validator_func( points )
 
     def test_variance_roi2(self):
         timestamp = 0.0
         framenumber = 0
         use_roi2 = True
         use_cmp = True
-        sz = FastImage.Size(self.w,self.h)
-        roi_im=FastImage.FastImage8u(sz)
-        self.ra.do_work(roi_im,timestamp,framenumber,use_roi2,use_cmp)
+        roi_im,result_validator_func=get_roi_im(self.w,self.h)
+        points = self.ra.do_work(roi_im,timestamp,framenumber,use_roi2,use_cmp)
+        result_validator_func( points )
 
 def get_test_suite():
     ts=unittest.TestSuite((unittest.makeSuite(TestRealtimeImageAnalysis),))
