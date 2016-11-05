762ccff66c22e8bb59818737e1bb10df870dd5b6
Andrew Straw <strawman@astraw.com>
2016-11-05 23:13:24 +0100

    statically link OpenCV

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ import pkg_resources # make sure FastImage is importable
 import motmot.FastImage.FastImage as FastImage
 import motmot.FastImage.util as FastImage_util
 
+cv_static_link=True
+
 # build with same IPP as FastImage
 major,minor,build = FastImage.get_IPP_version()
 vals = FastImage_util.get_build_info(ipp_arch=FastImage.get_IPP_arch(),
-                                     ipp_static=True)
+                                     ipp_static=FastImage.get_IPP_static())
 
 ipp_sources = vals.get('ipp_sources',[])
 ipp_include_dirs = vals.get('ipp_include_dirs',[])
@@ -45,9 +47,16 @@ cv_include_dirs = [os.path.join(cv_base,cv_inc_dir),
                   ]
 
 cv_libraries = ['opencv_core',
-                'opencv_legacy',
+                #'opencv_legacy',
                 'opencv_imgproc',
                 ]
+cv_extra_objects = []
+
+if cv_static_link:
+    build_dir = os.path.join(cv_base,'build')
+    lib_dir = os.path.join(build_dir,'lib')
+    cv_extra_objects = [os.path.join(lib_dir,'lib'+lib+'.a') for lib in cv_libraries]
+    cv_libraries = []
 
 ext_modules = []
 
@@ -66,7 +75,7 @@ if 1:
                                  define_macros=ipp_define_macros,
                                  extra_link_args=ipp_extra_link_args,
                                  extra_compile_args=ipp_extra_compile_args,
-                                 extra_objects=ipp_extra_objects,
+                                 extra_objects=ipp_extra_objects+cv_extra_objects,
                                  language="c++",
                                  ))
     ext_modules = cythonize(ext_modules)
