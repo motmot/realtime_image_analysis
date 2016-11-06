4f77b5123dac338646861d4ca022dde9f61b353d
Andrew Straw <strawman@astraw.com>
2016-11-06 10:45:41 +0100

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,10 @@ import motmot.FastImage.FastImage as FastImage
 import motmot.FastImage.util as FastImage_util
 
 # build with same IPP as FastImage
-major,minor,build = FastImage.get_IPP_version()
+ipp_root = os.environ['IPPROOT']
 vals = FastImage_util.get_build_info(ipp_arch=FastImage.get_IPP_arch(),
-                                     ipp_static=FastImage.get_IPP_static())
-
-ipp_sources = vals.get('ipp_sources',[])
-ipp_include_dirs = vals.get('ipp_include_dirs',[])
-ipp_library_dirs = vals.get('ipp_library_dirs',[])
-ipp_libraries = vals.get('ipp_libraries',[])
-ipp_define_macros = vals.get('ipp_define_macros',[])
-ipp_extra_link_args = vals.get('extra_link_args',[])
-ipp_extra_compile_args = vals.get('extra_compile_args',[])
-ipp_extra_objects = vals.get('ipp_extra_objects',[])
+                                     ipp_static=FastImage.get_IPP_static(),
+                                     ipp_root=ipp_root)
 
 ext_modules = []
 
@@ -29,16 +21,14 @@ if 1:
                                      'src/fic.c',
                                      'src/eigen.c',
                                      'src/c_time_time.c',
-                                     ]+ipp_sources
+                                     ]
     ext_modules.append(Extension(name='motmot.realtime_image_analysis.realtime_image_analysis',
                                  sources=realtime_image_analysis_sources,
-                                 include_dirs=ipp_include_dirs,
-                                 library_dirs=ipp_library_dirs,
-                                 libraries=ipp_libraries,
-                                 define_macros=ipp_define_macros,
-                                 extra_link_args=ipp_extra_link_args,
-                                 extra_compile_args=ipp_extra_compile_args,
-                                 extra_objects=ipp_extra_objects,
+                                 include_dirs=vals['ipp_include_dirs'],
+                                 library_dirs=vals['ipp_library_dirs'],
+                                 libraries=vals['ipp_libraries'],
+                                 define_macros=vals['ipp_define_macros'],
+                                 extra_objects=vals['ipp_extra_objects'],
                                  language="c++",
                                  ))
     ext_modules = cythonize(ext_modules)
